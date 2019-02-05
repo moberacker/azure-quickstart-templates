da5699653468fee323cc51442995a934215edf94
Markus Oberacker <markus.oberacker@ptv.de>
2019-02-05 17:00:24 +0100

    Update redis-cluster-install.sh

--- redis-high-availability/redis-cluster-install.sh
+++ redis-high-availability/redis-cluster-install.sh
@@ -459,7 +459,9 @@ configure_thp()
 	#issues with Redis. To fix this issue run the command 'echo never > /sys/kernel/mm/transparent_hugepage/enabled' as root, 
 	#and add it to your /etc/rc.local in order to retain the setting after a reboot. Redis must be restarted after THP is disabled.
  
-  	sudo echo "never" >> /sys/kernel/mm/transparent_hugepage/enabled
+  	sudo echo "never" > /sys/kernel/mm/transparent_hugepage/enabled
+	echo "sudo echo \"never\" > /sys/kernel/mm/transparent_hugepage/enabled" >> /etc/rc.local
+
 }
 
 #############################################################################
